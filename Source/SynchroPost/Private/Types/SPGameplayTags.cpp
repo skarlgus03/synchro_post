5a1ecc194c3c95f8ff33623e68af09c6669abe47
namgihyun <163094820+namugyun03@users.noreply.github.com>
2026-05-21 00:00:42 +0900

    [Feat] create SPGameplayTags, add Stat_Combat Tags

---
+++ Source/SynchroPost/Private/Types/SPGameplayTags.cpp
@@ -0,0 +1,45 @@
+
+#include "Types/SPGameplayTags.h"
+
+namespace SPTags
+{
+	namespace Stat
+	{
+		namespace Combat
+		{
+			namespace Primary
+			{
+				UE_DEFINE_GAMEPLAY_TAG(MaxHealth, "Stat.Combat.Primary.MaxHealth");
+
+				UE_DEFINE_GAMEPLAY_TAG(AtkPhysical, "Stat.Combat.Primary.AtkPhysical");
+				UE_DEFINE_GAMEPLAY_TAG(AtkMagic, "Stat.Combat.Primary.AtkMagic");
+
+				UE_DEFINE_GAMEPLAY_TAG(DefPhysical, "Stat.Combat.Primary.DefPhysical");
+				UE_DEFINE_GAMEPLAY_TAG(DefMagic, "Stat.Combat.Primary.DefMagic");
+
+				UE_DEFINE_GAMEPLAY_TAG(Speed, "Stat.Combat.Primary.Speed");
+			}
+			namespace Secondary
+			{
+				UE_DEFINE_GAMEPLAY_TAG(MaxAP, "Stat.Combat.Secondary.MaxAP");
+				UE_DEFINE_GAMEPLAY_TAG(MaxBP, "Stat.Combat.Secondary.MaxBP");
+
+				namespace Penetration
+				{
+					UE_DEFINE_GAMEPLAY_TAG(PhysicalFlat, "Stat.Combat.Secondary.Penetration.PhysicalFlat");
+					UE_DEFINE_GAMEPLAY_TAG(PhysicalPercent, "Stat.Combat.Secondary.Penetration.PhysicalPercent");
+					UE_DEFINE_GAMEPLAY_TAG(MagicFlat, "Stat.Combat.Secondary.Penetration.MagicFlat");
+					UE_DEFINE_GAMEPLAY_TAG(MagicPercent, "Stat.Combat.Secondary.Penetration.MagicPercent");
+				}
+
+				UE_DEFINE_GAMEPLAY_TAG(CritChance, "Stat.Combat.Secondary.CritChance");
+				UE_DEFINE_GAMEPLAY_TAG(CritDamage, "Stat.Combat.Secondary.CritDamage");
+
+				UE_DEFINE_GAMEPLAY_TAG(LifeSteal, "Stat.Combat.Secondary.LifeSteal");
+
+				UE_DEFINE_GAMEPLAY_TAG(Accuracy, "Stat.Combat.Secondary.Accuracy");
+				UE_DEFINE_GAMEPLAY_TAG(Evasion, "Stat.Combat.Secondary.Evasion");
+			}
+		}
+	}
+}
---
+++ Source/SynchroPost/Public/Types/SPGameplayTags.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+
+namespace SPTags
+{
+	namespace Stat
+	{
+		namespace Combat
+		{
+			namespace Primary
+			{
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(MaxHealth);
+
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(AtkPhysical);
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(AtkMagic);
+
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(DefPhysical);
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(DefMagic);
+
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(Speed);
+			}
+			namespace Secondary
+			{
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(MaxAP);
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(MaxBP);
+
+				namespace Penetration
+				{
+					UE_DECLARE_GAMEPLAY_TAG_EXTERN(PhysicalFlat);
+					UE_DECLARE_GAMEPLAY_TAG_EXTERN(PhysicalPercent);
+					UE_DECLARE_GAMEPLAY_TAG_EXTERN(MagicFlat);
+					UE_DECLARE_GAMEPLAY_TAG_EXTERN(MagicPercent);
+				}
+
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(CritChance);
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(CritDamage);
+
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(LifeSteal);
+
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(Accuracy);
+				UE_DECLARE_GAMEPLAY_TAG_EXTERN(Evasion);
+			}
+
+		}
+	}
+}
